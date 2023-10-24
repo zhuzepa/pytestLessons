871801a9c5075ef2f08ec37609d5e03a16877e12
Prince Aleksey <chirkinaa@pgk.ru>
2023-10-24 23:32:51 +0300

    add 2 test

---
+++ tests/test_mail.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def set_up():
+    print('Вход в систему выполнен')
+
+
+def test_send_mail_1(set_up):
+    print('Письмо отправлено')
+
+
+def test_send_mail_2(set_up):
+    print('Письмо отправлено')
